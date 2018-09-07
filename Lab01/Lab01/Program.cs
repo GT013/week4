c022fb7a2f894310d09a714b9fdeb77f8ba61217
nantikan <60030049@kmitl.ac.th>
2018-09-07 13:46:28 +0700

    add data4.1

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,8 +6,7 @@ namespace Lab01
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.Write("Hello");
+        {        
             Console.WriteLine("This is text 1.");
             Console.WriteLine("This is text 2.");
             Console.WriteLine("This is text 3.");
