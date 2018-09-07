7b62b11783d6bf2c0c89ae127b88d17cabcbd909
nantikan <60030049@kmitl.ac.th>
2018-09-07 15:47:46 +0700

    add data17

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,8 +7,14 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            
+            int a, b, c, d, e, f;
+            a = 1;
+            b = a + 6;
+            c = b - 3;
+            d = c * 2;
+            e = d / 2;
+            f = e % 2;
+
         }
     }
 }
