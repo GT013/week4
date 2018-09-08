4cf55b8501b294f89ed2af78993f7589c8d6ac24
nantikan <60030049@kmitl.ac.th>
2018-09-08 22:56:57 +0700

    complete19

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,8 +7,18 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            
+            const double kmToau = 0.000000006685;
+            const double kmTomile = 0.621371192;
+            const double lightSpeed = 186000d;   // miles per second
+            double lighttime, sunto,dis,sum;
+            Console.Write("Please Distance = ");
+            dis = double.Parse(Console.ReadLine());
+            sum = dis * kmToau;
+            Console.WriteLine("DistanceToSun = {0:F2} A.U",sum);
+            sunto = dis * kmTomile;
+            Console.WriteLine("DistanceMile = {0:F2} Mile",sunto);
+            lighttime = sunto / lightSpeed;
+            Console.WriteLine("LightSpeedTime = {0:F2} Minutes",lighttime/60);
         }
     }
 }
